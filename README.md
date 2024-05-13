ccda02134ba82d2d841e8367fbf8b4f385ee154c
Alex <alex@abarthel.me>
2024-05-13 18:06:04 +0200

    initial release

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# WindowsGSM.teratechworlds
+# WindowsGSM.TerraTechWorlds
+🧩WindowsGSM plugin that provides TerraTechWorlds Dedicated server
+
+## PLEASE ⭐STAR⭐ THE REPO IF YOU LIKE IT! THANKS!
+
+### WindowsGSM Installation: 
+1. Download  WindowsGSM https://windowsgsm.com/ 
+2. Create a Folder at a Location you wan't all Server to be Installed and Run.
+3. Drag WindowsGSM.Exe into previously created folder and execute it.
+
+### Plugin Installation:
+1. Download [latest](https://https://github.com/Raziel7893/WindowsGSM.terratechworlds/releases/latest) release
+2. Either Extract then Move the folder **TerraTechWorlds.cs** to **WindowsGSM/plugins** 
+    1. Press on the Puzzle Icon in the left bottom side and press **[RELOAD PLUGINS]** or restart WindowsGSM
+3. Or Press on the Puzzle Icon in the left bottom side and press **[IMPORT PLUGIN]** and choose the downloaded .zip
+
+### Official Documentation
+🗃️ Didn't find any documentation yet. Please Let me know if you came accros one
+
+### The Game
+🕹️ https://store.steampowered.com/app/2313330/TerraTech_Worlds/
+
+### Dedicated server info
+🖥️ https://steamdb.info/app/2321660/
+
+### Port Forwarding (YOU NEED THIS, TO BE ABLE TO CONNECT FROM THE INTERNET(only for servers/pcs at home):
+- If You don't know How: Google: YourRouterBrand + Portforwarding ( https://portforward.com/ can also help)
+- 7777 UDP - Default
+
+### Files To Backup
+- Save Gane (You could only save serverfiles/TerraTechWorlds/Saved , but that includes many big logs)
+  - WindowsGSM\servers\%ID%\serverfiles/TT2/Saved/SaveGames
+  - WindowsGSM\servers\%ID%\serverfiles/TerraTechWorlds/Saved/Config
+- WindowsGSM Config
+  - WindowsGSM\servers\%ID%\configs
+
+### Available Settings
+All these params are automatically set by WGSM
+- port=8211                     can be change and working (Change via WGSM settings)
+- slotCount=MaxPlayers			can be change and working (Change via WGSM settings) 
+- password=""					can be changed by Browse=>ServerFiles=>dedicated_server_config.json
+
+### Other notes
+- The game is currently in Early Access Stage WGSM and this plugin is not taking liability if something happens to your server, the app is only for managing your server easily
+
+### How can you play with your friends without port forwarding?
+- Use [zerotier](https://www.zerotier.com/) folow the basic guide and create network
+- Download the client app and join to your network
+- Create static IP address for your host machine
+- Edit WGSM IP Address to your recently created static IP address
+- Give your network ID to your friends
+- After they've joined to your network
+- They can connect using the IP you've created eg: 10.123.17.1:7777
+- Enjoy
+
+### Support
+[WGSM](https://discord.com/channels/590590698907107340/645730252672335893)
+
+### Give Love!
+[Buy me a coffee](https://ko-fi.com/raziel7893)
+
+[Paypal](https://paypal.me/raziel7893)
+
+### License
+This project is licensed under the MIT License - see the <a href="https://github.com/raziel7893/WindowsGSM.TerraTechWorlds/blob/main/LICENSE">LICENSE.md</a> file for details
+
+### Thanks
+Thanks to ohmcodes for the Enshrouded and Palworld Plugins which i used for guidance to create this one
---
+++ TerraTechWorlds.cs/TerraTechWorlds.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using WindowsGSM.Functions;
+using WindowsGSM.GameServer.Query;
+using WindowsGSM.GameServer.Engine;
+using System.IO;
+using System.Text;
+
+namespace WindowsGSM.Plugins
+{
+    public class TerraTechWorlds : SteamCMDAgent
+    {
+        // - Plugin Details
+        public Plugin Plugin = new Plugin
+        {
+            name = "WindowsGSM.TerraTechWorlds", // WindowsGSM.XXXX
+            author = "raziel7893",
+            description = "WindowsGSM plugin for supporting TerraTechWorlds Dedicated Server",
+            version = "1.0.0",
+            url = "https://github.com/Raziel7893/WindowsGSM.terratechworlds", // Github repository link (Best practice) TODO
+            color = "#34FFeb" // Color Hex
+        };
+
+        // - Settings properties for SteamCMD installer
+        public override bool loginAnonymous => true;
+        public override string AppId => "2321660"; // Game server appId Steam
+
+        // - Standard Constructor and properties
+        public TerraTechWorlds(ServerConfig serverData) : base(serverData) => base.serverData = _serverData = serverData;
+        private readonly ServerConfig _serverData;
+
+
+        // - Game server Fixed variables
+        public override string StartPath => "TT2\\Binaries\\Win64\\TT2Server-Win64-Shipping.exe";
+        public string FullName = "TerraTechWorlds Dedicated Server"; // Game server FullName
+        public string ConfigFile = "dedicated_server_config.json"; // Game server FullName
+
+        public bool AllowsEmbedConsole = true;  // Does this server support output redirect?
+        public int PortIncrements = 1; // This tells WindowsGSM how many ports should skip after installation
+
+        // - Game server default values
+        public string Port = "7777"; // Default port
+
+        public string Additional = "-log"; // Additional server start parameter
+
+        // TODO: Following options are not supported yet, as ther is no documentation of available options
+        public string Maxplayers = "16"; // Default maxplayers        
+        public string QueryPort = "7778"; // Default query port. This is the port specified in the Server Manager in the client UI to establish a server connection.
+        // TODO: Unsupported option
+        public string Defaultmap = "Dedicated"; // Default map name
+        // TODO: Undisclosed method
+        public object QueryMethod = new A2S(); // Query method should be use on current server type. Accepted value: null or new A2S() or new FIVEM() or new UT3()
+
+
+        public void UpdateConfig()
+        {
+            string configFile = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, ConfigFile);
+            StringBuilder sb = new StringBuilder();
+            if (!File.Exists(configFile))
+            {
+                sb.Append($"{{" +
+                    $"\r\n  \"Port\": 7777,  " +
+                    $"\r\n  \"SlotCount\": 6," +
+                    $"\r\n  \"Password\": \"\"" +
+                    $"\r\n}}");
+            }
+            else
+            {
+                StreamReader sr = new StreamReader(configFile);
+                var line = sr.ReadLine();
+                while (line != null)
+                {
+                    if (line.Contains("Port\":"))
+                        sb.Append($"\r\n  \"Port\": {serverData.ServerPort},  ");
+                    else if (line.Contains("SlotCount\":"))
+                        sb.Append($"\r\n  \"SlotCount\": {serverData.ServerMaxPlayer},");
+                    else
+                        sb.Append(line);
+                    line = sr.ReadLine();
+                }
+            }
+
+            File.WriteAllText(configFile, sb.ToString());
+        }
+
+
+        // - Start server function, return its Process to WindowsGSM
+        public async Task<Process> Start()
+        {
+            string shipExePath = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath);
+            if (!File.Exists(shipExePath))
+            {
+                Error = $"{Path.GetFileName(shipExePath)} not found ({shipExePath})";
+                return null;
+            }
+
+            StringBuilder param = new StringBuilder();
+            param.Append($" {serverData.ServerParam}");
+
+            // Prepare Process
+            var p = new Process
+            {
+                StartInfo =
+                {
+                    CreateNoWindow = false,
+                    WorkingDirectory = ServerPath.GetServersServerFiles(_serverData.ServerID),
+                    FileName = shipExePath,
+                    Arguments = param.ToString(),
+                    WindowStyle = ProcessWindowStyle.Minimized,
+                    UseShellExecute = false,
+                },
+                EnableRaisingEvents = true
+            };
+
+            // Set up Redirect Input and Output to WindowsGSM Console if EmbedConsole is on
+            if (_serverData.EmbedConsole)
+            {
+                p.StartInfo.RedirectStandardInput = true;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardError = true;
+                p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                p.StartInfo.CreateNoWindow = true;
+                var serverConsole = new ServerConsole(_serverData.ServerID);
+                p.OutputDataReceived += serverConsole.AddOutput;
+                p.ErrorDataReceived += serverConsole.AddOutput;
+            }
+
+            // Start Process
+            try
+            {
+                p.Start();
+                if (_serverData.EmbedConsole)
+                {
+                    p.BeginOutputReadLine();
+                    p.BeginErrorReadLine();
+                }
+                return p;
+            }
+            catch (Exception e)
+            {
+                Error = e.Message;
+                return null; // return null if fail to start
+            }
+        }
+
+        // - Stop server function
+        public async Task Stop(Process p)
+        {
+            await Task.Run(() =>
+            {
+                Functions.ServerConsole.SetMainWindow(p.MainWindowHandle);
+                Functions.ServerConsole.SendWaitToMainWindow("^c");
+                p.WaitForExit(5000);
+                if (!p.HasExited)
+                    p.Kill();
+            });
+        }
+    }
+}
